603558d622dcc7accbd70132daaa1ef53b819465
Mathew Merryweather <mathew.merryweather@me.com>
2017-10-01 21:42:36 +1100

    Update apply_rosin_rammler.R

--- sizing/apply_rosin_rammler.R
+++ sizing/apply_rosin_rammler.R
@@ -1,6 +1,7 @@
-apply_rosin_rammler = function(size, k, n ){
+apply_rosin_rammler = function(size, RR){
   # size in mm
-  # k is the Rosin-Rammler P80 parameter and
-  # n is the Rosin-Rammler spread parameter
-    100*(1-exp(-((size/k)^n)))
+  # RR is assumed to be a data frame with 1 observation of two values:
+  # RR_k is the Rosin-Rammler P80 parameter and
+  # RR_n is the Rosin-Rammler spread parameter
+    100*(1-exp(-((size/RR$RR_k)^RR$RR_n)))
 }
