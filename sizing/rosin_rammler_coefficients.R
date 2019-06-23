aa33e87dd6ca9a22f523affd7a0aad954f824113
Mathew Merryweather <mathew.merryweather@me.com>
2019-06-23 15:41:19 +1000

    Update rosin_rammler_coefficients.R

--- sizing/rosin_rammler_coefficients.R
+++ sizing/rosin_rammler_coefficients.R
@@ -1,13 +1,16 @@
-rosin_rammler_coefficients = function(tbl){
-    pacman::p_load(tidyverse)
-    RR = tbl %>% mutate(x = log(bottom_size),
-                        y = log(-log(1-mass_passing/100)))
-    RR %>%
-        filter(y != Inf & x != -Inf) %>%
-        lm(y ~ x, data = .) %>%
-        broom::tidy() %>%
-        select(term, estimate) %>%
-        spread(term,estimate) %>%
-        set_names(c("Intercept","RR_n")) %>%
-        transmute(RR_n, RR_k = exp(-Intercept/RR_n))
+rosin_rammler_coefficients = function(tbl, bottom_size, mass_passing){
+  pacman::p_load(tidyverse)
+  size_enq = enquo(bottom_size)
+  mass_enq = enquo(mass_passing)
+  RR = tbl %>%
+    mutate(x = log(!!size_enq),
+           y = log(-log(1 - !!mass_enq / 100)))
+  RR %>%
+    filter(y != Inf & x != -Inf) %>%
+    lm(y ~ x, data = .) %>%
+    broom::tidy() %>%
+    select(term, estimate) %>%
+    spread(term,estimate) %>%
+    set_names(c("Intercept","RR_n")) %>%
+    transmute(RR_n, RR_k = exp(-Intercept/RR_n))
 }
